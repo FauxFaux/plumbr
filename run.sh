19cd7400aa14c0f1dfe29dfc037771851c1ae1b4
Nikita Salnikov-Tarnovski <nikem@plumbr.eu>
2014-12-15 13:22:12 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p build/classes
-javac -d build/classes src/main/java/slow/Main.java src/main/java/slow/Worker.java src/main/java/slow/Cache.java src/main/java/slow/internal/Source.java
-java -cp build/classes slow.Main
+javac -d build/classes src/main/java/slow/internal/Main.java src/main/java/slow/Accumulator.java src/main/java/slow/Worker.java src/main/java/slow/Cache.java src/main/java/slow/internal/Source.java
+java -cp build/classes slow.internal.Main
